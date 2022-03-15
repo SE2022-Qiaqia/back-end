295b617ab9bbccfa7b2c2b39de470f4859a2b2ce
SalHe Li <SalHe@qq.com>
2022-03-16 01:09:25 +0800

    init

---
+++ src/main/kotlin/com/whu/se2022/qiaqia/coursesystem/CourseSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.whu.se2022.qiaqia.coursesystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CourseSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CourseSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/whu/se2022/qiaqia/coursesystem/CourseSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.whu.se2022.qiaqia.coursesystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
