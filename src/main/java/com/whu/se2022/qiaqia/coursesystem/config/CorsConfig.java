2c55a49ec2c543e1ca09ef809b3007b370580503
SalHe Li <SalHe@qq.com>
2022-03-17 00:07:13 +0800

    添加一个简单的Controller，简单配置跨域及Security

---
+++ src/main/java/com/whu/se2022/qiaqia/coursesystem/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.whu.se2022.qiaqia.coursesystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
