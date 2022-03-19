9789ed79861ae91e8a21cf6b89e8ad842a85c888
SalHe Li <SalHe@qq.com>
2022-03-19 16:38:09 +0800

    集成Docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,77 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.Properties
 
 plugins {
-	id("org.springframework.boot") version "2.6.4"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+    id("org.springframework.boot") version "2.6.4"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+    id("com.bmuschko.docker-spring-boot-application") version "7.3.0"
+    id("com.palantir.git-version") version "0.13.0"
 }
 
 group = "com.whu.se2022.qiaqia"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+val gitDetails = run {
+    val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+    versionDetails()
+}
+
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+docker {
+    val dockerProperties = Properties().apply {
+        val propertiesFile = rootProject.file("gradle-docker.properties")
+        if (propertiesFile.exists())
+            load(propertiesFile.inputStream())
+        this.putAll(System.getProperties())
+    }
+    val dockerRegistryUrl = dockerProperties.getProperty("docker.registry.url")
+    val dockerRegistryUsername = dockerProperties.getProperty("docker.registry.username")
+    val dockerRegistryPassword = dockerProperties.getProperty("docker.registry.password")
+    val dockerImageName = dockerProperties.getProperty("docker.image.name.backend")
+
+    dockerProperties.getProperty("docker.remote.api.address")?.let { url.set(it) }
+    springBootApplication {
+        baseImage.set("openjdk:11")
+        images.addAll("$dockerImageName:nightly", "$dockerImageName:${gitDetails.gitHash}")
+    }
+    registryCredentials {
+        url.set(dockerRegistryUrl)
+        username.set(dockerRegistryUsername)
+        password.set(dockerRegistryPassword)
+    }
 }
